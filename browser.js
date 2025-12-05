efd989fcb10bf0e90930e67878dc517916dcfd6e
Frederic <1665799+Finomosec@users.noreply.github.com>
2025-12-05 21:22:03 +0100

    Updated browser.js

--- browser.js
+++ browser.js
@@ -58,7 +58,7 @@ async function initBrowser({ host = '127.0.0.1', port = 9222, dishyUrl = 'http:/
     );
   }
 
-  const userDataDir = `/tmp/dishy-chrome-${process.pid}`;
+  const userDataDir = `$HOME/Projects/starlink-prometheus-exporter`;
   const chromeArgs = [
     `--remote-debugging-port=${cdpPort}`,
     '--remote-debugging-address=0.0.0.0',
